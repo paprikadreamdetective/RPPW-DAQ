8c11656eaa652f166c48459e01409a543dcb799e
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-03-13 23:26:49 -0600

    WiFi is up

--- WLAN_connection.py
+++ WLAN_connection.py
@@ -3,9 +3,10 @@ import time
 from machine import Pin
 from umqtt.simple import MQTTClient
 
-def init_connection(ssid: str, key: str):
+def init_connection(ssid: str, key: str) -> network:
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, key)
-    time.sleep(5)
-    print(wlan.isconnected())
+    return wlan
+    #print(wlan.isconnected())
+    #time.sleep(5)
