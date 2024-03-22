37dad2b33bf18c422b445e0842887475010c214f
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-03-22 10:31:23 -0600

    add correlation factors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from model.SocketConnector import SocketConnector 
+from model.MQ135 import MQ135
+
 import time
 import machine
 multicast_group = '224.10.10.5' 
@@ -6,22 +8,33 @@ port = 10000
 print('Connecting to ' + str(multicast_group) + ' : ' + str(port))
 led = machine.Pin('LED', machine.Pin.OUT)
 
-analog_pin = machine.ADC(28)
+temperature = 7.0
+humidity = 61.0
+
+mq135 = MQ135(28)
+
+#analog_pin = machine.ADC(28)
 
 web_socket_sender = SocketConnector(multicast_group, port)
 
-def read_mq135():
-  adc_value = analog_pin.read_u16()
-  #mq135_ppm = adc_value * (3300 / 65535.0)
-  mq135_ppm = adc_value
-  return mq135_ppm
-
 while True:
   #message = b'Im Raspberry Pi Pico W, Whats u r name?'
-  message = str(read_mq135())
-  print('Lectura del sensor MQ-135: ' + message + ' ppm')
-  print('Sending {!r}'.format(message))
-  web_socket_sender.send_msg(message)
+
+  rzero = mq135.get_rzero()
+  corrected_rzero = mq135.get_corrected_rzero(temperature, humidity)
+  resistance = mq135.get_resistance()
+  ppm = mq135.get_ppm()
+  corrected_ppm = mq135.get_corrected_ppm(temperature, humidity)
+
+  print("MQ135 RZero: " + str(rzero) +"\t Corrected RZero: "+ str(corrected_rzero)+
+  "\t Resistance: "+ str(resistance) +"\t PPM: "+str(ppm)+
+  "\t Corrected PPM: "+str(corrected_ppm)+"ppm")
+
+
+  #message = str(read_mq135())
+  #print('Lectura del sensor MQ-135: ' + message + ' ppm')
+  #print('Sending {!r}'.format(message))
+  #web_socket_sender.send_msg(message)
   led.off()
   time.sleep_ms(500)
   led.on()
--- model/Sensor.py
+++ model/Sensor.py
@@ -1,3 +1,21 @@
 class Sensor:
-    def read(self):
+    def get_correction_factor(self, temperature, humidity):
+       raise NotImplementedError()
+
+    def get_resistance(self):
+       raise NotImplementedError()
+
+    def get_corrected_resistance(self, temperature, humidity):
+       raise NotImplementedError()
+
+    def get_ppm(self):
+        raise NotImplementedError()
+
+    def get_corrected_ppm(self, temperature, humidity):
+        raise NotImplementedError()
+
+    def get_rzero(self):
+       raise NotImplementedError()
+
+    def get_corrected_rzero(self, temperature, humidity):
         raise NotImplementedError()
