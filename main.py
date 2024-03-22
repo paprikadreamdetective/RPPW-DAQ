da6e5c51440d0d99cb3802084caaaec18dff3273
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-03-21 22:44:50 -0600

    fix delay in main

--- main.py
+++ main.py
@@ -1,63 +1,17 @@
-
-#import socket
 from model.SocketConnector import SocketConnector 
 import time
-multicast_group = ('224.10.10.5', 10000)
-
-#sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-#server_address = ('192.168.100.136', 999)
-
-print('Connecting to {} port {}'.format(*multicast_group))
-
-web_socket_sender = SocketConnector('224.10.10.5', 10000)
-
-
-#sck.connect(server_address)
+import machine
+multicast_group = '224.10.10.5' 
+port = 10000
+print('Connecting to ' + str(multicast_group) + ' : ' + str(port))
+led = machine.Pin('LED', machine.Pin.OUT)
+web_socket_sender = SocketConnector(multicast_group, port)
 
 while True:
-#try:
   message = b'Im Raspberry Pi Pico W, Whats u r name?'
-  print('sending {!r}'.format(message))
+  print('Sending {!r}'.format(message))
   web_socket_sender.send_msg(message)
-  time.sleep(1)
-    #sent = sck.sendto(message, multicast_group)
-    #sck.sendall(message)
-    
-    #amount_received = 0
-    #amount_expected = len(message)
-    #while amount_received < amount_expected:
-    #    data = sck.recv(32)
-    #    amount_received += len(data)
-    #    print('received {!r}'.format(data))
-#finally:
-#    print('closing socket')
-#    sck.close()
+  led.off()
+  time.sleep_ms(500)
+  led.on()
   
-
-'''
-import socket
-import time
-sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('192.168.100.136', 999)
-
-print('connecting to {} port {}'.format(*server_address))
-sck.connect(server_address)
-
-while True:
-  #try:
-  message = b'It is very long message but will only be transmitted in chunks of 16 at a time'
-  print('sending {!r}'.format(message))
-  sck.sendall(message)
-  amount_received = 0
-  amount_expected = len(message)
-    #while amount_received < amount_expected:
-    #    data = sck.recv(32)
-    #    amount_received += len(data)
-    #    print('received {!r}'.format(data))
-  #finally:
-    #print('closing socket')
-    #sck.close()
-  time.sleep(10)
-'''
