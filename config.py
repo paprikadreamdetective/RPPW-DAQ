953f1cb10fb4273ac2e8b472ec920dedd337bb58
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-03-29 02:23:45 -0600

    config is OK

--- config.py
+++ config.py
@@ -3,9 +3,6 @@
 configuration variables for wlan credentials
 
 """
-#"ssid" : '[ascii values]'
-#"pswd" : '[ascii values]'
-
 cfg = {
     "wlan": {
         "ssid" : [84, 111, 116, 97, 108, 112, 108, 97, 121, 45, 54, 53, 65, 53],
