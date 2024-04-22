eb5e20ff6abda763800bc29245066a0d86998e9b
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-04-22 15:56:51 -0600

    implements absmethods in command handler

--- model/commandhandler.py
+++ model/commandhandler.py
@@ -1,4 +1,5 @@
 import machine
+import utime
 from .messagehandler import MessageHandler
 from .communicationhandler import CommunicationHandler
 class CommandHandler(MessageHandler, CommunicationHandler):
@@ -21,5 +22,42 @@ class CommandHandler(MessageHandler, CommunicationHandler):
         """
         input_string = ''
         while machine.uart.any():
-            pass
+            in_char = chr(machine.uart.read(1)[0])
+            input_string += in_char
+            if in_char == '!':
+                new_command = True
+                break
+        return input_string
+    def parse_serial_slave(new_command: bool) -> str:
+        """
+        Lectura serial del buffer
+        """
+        input_string = ''
+        while machine.uart.any():
+            in_char = chr(machine.uart.read(1)[0])
+            input_string += in_char
+            if in_char == '!':
+                new_command = True
+                break
+        return input_string
+    def parse_slave_to_master(self) -> str:
+        """
+        Esta es una funcion que bloquea.
+
+        Espera 90ms la entrada del buffer serial de los esclavos (Serial2)
+        Esta lectura se hace cuando el buffer de informacion Serial2 esta disponible
+        """
+        wait_for = 500
+        waiting = True
+        slave_string = ''
+        started_waiting = utime.ticks_ms()
+        while waiting and (utime.ticks_diff(utime.ticks_ms(), started_waiting) <= wait_for):
+            while uart.any():
+                in_char = uart.read(1)[0]
+                if in_char != '!':
+                    slave_string += in_char
+                if in_char == '!':
+                    waiting = False
+                    break
+        return slave_string
 
