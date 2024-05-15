c8cb10fee9c930f88f72951d9d7b9365e34c2ba8
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-05-15 14:02:10 -0600

    delete unnecessary files

--- MQ135.py
+++
@@ -1,9 +0,0 @@
-import Sensor 
-import machine
-
-class MQ135(Sensor.Sensor):
-    def __init__(self, pin):
-        self.pin = machine.ADC(pin)
-        
-    def read(self) -> int:
-        return self.pin.read_u16()
