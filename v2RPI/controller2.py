9a152b93a48e1c176b81bf9db245b41a09d610ed
paprikadreamdetective <t4v0sg@gmail.com>
2024-10-23 18:08:11 -0600

    add new config

--- v2RPI/controller2.py
+++ v2RPI/controller2.py
@@ -10,18 +10,9 @@ import threading
 import numpy as np
 from adafruit_ahtx0 import AHTx0
 import json
+from mcp3008 import ADC_MCP3008
 
 app = create_flask_app()
-
-one_second = 1
-
-TIMER_1 = False
-samples_per_second_timer_1 = 4
-cycle_time_timer_1 = one_second / samples_per_second_timer_1
-
-TIMER_2 = False
-samples_per_second_timer_2 = 1
-cycle_time_timer_2 = one_second / samples_per_second_timer_2
 
 @app.route('/set_mode_manual', methods=['POST'])
 def pwm_set_mode():
@@ -42,21 +33,19 @@ def timer_1_callback():
     adc_analog_inputs = master.getAnalogChannelValues() 
     print("TIMER 1: ", time.ctime())
     print(adc_analog_inputs)
-    threading.Timer(cycle_time_timer_1, timer_1_callback).start()
+    threading.Timer(0.25, timer_1_callback).start()
 
 def timer_2_callback():
     i2c_inputs = [{'temperature' : i2c_sensor[sensor].temperature, 'humidity' : i2c_sensor[sensor].relative_humidity} for sensor in i2c_sensor] 
     print("TIMER 2: ", time.ctime())
     print(i2c_inputs)
-    threading.Timer(cycle_time_timer_2, timer_2_callback).start()
+    threading.Timer(1, timer_2_callback).start()
 
 def init_timers():
     timer_1_callback()
     timer_2_callback()
 
 def daq_task():
-    #global TIMER_1
-    #global TIMER_2
     global master
     global i2c_sensor
     i2c = busio.I2C(board.SCL, board.SDA)
@@ -65,20 +54,10 @@ def daq_task():
     adc = ADC_MCP3008(busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI), digitalio.DigitalInOut(board.D8))
     master = create_master_daq(adc, [], [])
     master.initOutputs('config.json')  
-    time.sleep(3)
     try:
         init_timers()
-        #init_outputs()
         while 1:
-            #output_ch0.write_output(adc_analog_inputs[0]['CH0'])
             master.writeAllOutputPWM()
-            # if TIMER_1:
-                
-                # output_ch0.write_output(adc_analog_inputs[0]['CH0'])
-                # output_ch0.write_output(ADC['CH0'].value)
-                # TIMER_1 = False
-            #if TIMER_2:
-            #    TIMER_2 = False    
     except KeyboardInterrupt:
         print("Programa detenido por el usuario.")
         #output_ch0.cleanup()
