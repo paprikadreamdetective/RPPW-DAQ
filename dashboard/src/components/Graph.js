ed7b6f834bffb59a1b52815f8df774cb97900735
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-11-28 21:17:58 -0600

    add first version of motor controller

--- dashboard/src/components/Graph.js
+++ dashboard/src/components/Graph.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Graph.css';
+import MotorControl from './MotorControl';
 
 function Graph({ graphUrl }) {
   return (
@@ -7,16 +8,20 @@ function Graph({ graphUrl }) {
       <h3>Contenedor 2: Charts</h3>
       <div className="charts-content">
         <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <iframe
+          {/*<iframe
             src={graphUrl}
             title="Zabbix Graph"
             width="1000"
             height="200"
             frameBorder="0"
             allowFullScreen
-          />
+          />*/}
+          
+          <MotorControl></MotorControl>
         </div>
+        
       </div>
+      
     </div>
   );
 }
---
+++ dashboard/src/components/MotorControl.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./MotorControl.css";
+
+const MotorControl = () => {
+  const [speed, setSpeed] = useState("");
+  const [revolutions, setRevolutions] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      speed: parseInt(speed),
+      revolutions: parseInt(revolutions),
+    };
+
+    try {
+      const response = await fetch("http://192.168.100.164:5000/control_motor", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const responseData = await response.json();
+      window.confirm(responseData.message);
+    } catch (error) {
+      setError("Error al enviar la solicitud.");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Controlador de Arduino</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="number"
+          className="input"
+          placeholder="Velocidad (RPM)"
+          value={speed}
+          onChange={(e) => setSpeed(e.target.value)}
+        />
+        <input
+          type="number"
+          className="input"
+          placeholder="Revoluciones"
+          value={revolutions}
+          onChange={(e) => setRevolutions(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Enviar Comando
+        </button>
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default MotorControl;
