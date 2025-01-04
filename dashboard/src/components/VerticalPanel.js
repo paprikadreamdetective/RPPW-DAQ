f7290f05f86e3f4aa08b98ad9a7e65fe6e677022
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2025-01-03 18:39:26 -0600

    🎉 Vertical slider for step motor is up
    Vertical slider for step motor is up

--- dashboard/src/components/VerticalPanel.js
+++ dashboard/src/components/VerticalPanel.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import "./VerticalPanel.css";
-
+import MotorControl from "./MotorControl";
 function VerticalSwipeToSlide() {
   const settings = {
     dots: false,
@@ -11,30 +11,37 @@ function VerticalSwipeToSlide() {
     vertical: true,
     verticalSwiping: true,
     swipeToSlide: true,
-    arrows: true // Activa las flechas de navegación
-    /*beforeChange: function(currentSlide, nextSlide) {
+    arrows: true, // Activa las flechas de navegación
+    swipe: false, // Deshabilita el deslizamiento con el mouse o touch
+    draggable: false, // Deshabilita arrastrar el slider
+    beforeChange: function(currentSlide, nextSlide) {
       console.log("before change", currentSlide, nextSlide);
     },
     afterChange: function(currentSlide) {
       console.log("after change", currentSlide);
-    }*/
+    }
   };
   return (
     <>
     {/*<div className="slider-container">*/}
     
     <div className="vertical-panel-slider">
+    <div className="slider-container">
       <Slider {...settings}>
         <div className="vertical-panel-container">
-          <h3>1</h3>
+          <h3>Motor X</h3>
+          <MotorControl></MotorControl>
         </div>
         <div className="vertical-panel-container">
-          <h3>2</h3>
+          <h3>Motor Y</h3>
+          <MotorControl></MotorControl>
         </div >
         <div className="vertical-panel-container">
-          <h3>3</h3>
+          <h3>Motor Z</h3>
+          <MotorControl></MotorControl>
         </div>
       </Slider>
+    </div>
     </div>
     </>
   );
